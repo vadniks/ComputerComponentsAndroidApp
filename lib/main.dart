3be023268e96904887e8e1612bb0379f3f55f77c
vadniks <whatever@fake.some>
2023-03-26 18:20:43 +0300

    Beginning

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.dark(useMaterial3: true),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
